4e8ecc8efe8dc37348d19d70d281936961f8838b
Wanghao Wei <quic_wanghaow@quicinc.com>
2021-01-13 20:15:10 +0800

    Add QRB5165 tflite_segmentation Sample Code
    Signed-off-by: Wanghao Wei <quic_wanghaow@quicinc.com>

---
+++ tflite_segmentation/install.sh
@@ -0,0 +1,10 @@
+# Copyright (c) 2020 Qualcomm Innovation Center, Inc.  All Rights Reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+# weston.sh: script to start weston display server
+cp weston.sh /data
+
+# TFLite segmentation model 
+cp dv3_argmax_int32.tflite /data/misc/camera
+
+
