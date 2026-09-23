c2c9599699c0d6261b2adc04831c7a9e38450282
quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>
2026-09-23 10:54:07 +0530

    Update weston.sh
    Signed-off-by: quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>

--- RB5/linux_kernel_4_x/GStreamer-apps/python/weston.sh
+++ RB5/linux_kernel_4_x/GStreamer-apps/python/weston.sh
@@ -1,3 +1,6 @@
+# Copyright (c) Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
 export XDG_RUNTIME_DIR=/dev/socket/weston
 mkdir -p /dev/socket/weston
 /usr/bin/weston --tty=1 --idle-time=0 &
