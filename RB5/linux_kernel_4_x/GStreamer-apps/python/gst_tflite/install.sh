a20618ac9ecc66d32982d118a36d086dc9e3f9e3
quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>
2026-09-23 10:54:37 +0530

    Update install.sh
    Signed-off-by: quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>

--- RB5/linux_kernel_4_x/GStreamer-apps/python/gst_tflite/install.sh
+++ RB5/linux_kernel_4_x/GStreamer-apps/python/gst_tflite/install.sh
@@ -1,3 +1,6 @@
+# Copyright (c) Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
 rm -rf detect.tflite labelmap.txt
 wget https://raw.githubusercontent.com/quic/sample-apps-for-Qualcomm-Robotics-RB5-platform/master/Gstreamer-Applications/gst_tflite/labelmap.txt
 wget https://raw.githubusercontent.com/quic/sample-apps-for-Qualcomm-Robotics-RB5-platform/master/Gstreamer-Applications/gst_tflite/detect.tflite
